47bc30e1c0d55b01d59ec83b8f878008dceb8e34
unknown <awalupshot@gmail.com>
2022-05-11 01:00:05 +0545

    adasdartrt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '90px' }} />
+        <img src={Logo} alt='Logos' style={{ width: '90px' }} />
       </div>
 
       {/* menu */}
